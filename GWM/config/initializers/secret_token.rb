1f880f79baefc0e777c95bdfbc4d2b3a3f12763d
MattPua <matt.pua@utoronto.ca>
2013-07-01 00:53:19 -0400

    first push

---
+++ GWM/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GWM::Application.config.secret_token = 'ff3341740e69f78c95a1ad89ed16956e1dc685708f84c3d12e424fa6a60cec4ca2f31d14ea5c28f2abc387ee27112b6d21712ab352014b466ce552b97542dc0b'
